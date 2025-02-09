8ab1e72fdc36f7a1b1f658bfb9113103d8338b1c
Faysal Ahmed <info.faysal.32@gmail.com>
2025-02-09 15:59:03 +0600

    loop in js

---
+++ Milestone 4/Module 19/3While.js
@@ -0,0 +1,14 @@
+// let num = 1,sum=0;
+// while(num <= 10) {
+//     sum+=num;
+//     num++;
+// }
+// console.log(sum);
+
+let numbers = 1;
+while (numbers <= 10) {
+  if (numbers % 2 == 0) {
+    console.log(numbers);
+  }
+  numbers++;
+}
---
+++ Milestone 4/Module 19/4For.js
@@ -0,0 +1,9 @@
+let sum=0;
+for(let i=1; i<=10; i++) {
+    sum+=i;
+}
+console.log(sum);
+
+for(let i=10;i>=0;i--) {
+    console.log(i);
+}
